cc3f5ac3a429aa367c3c1f34e443c6072bd400eb
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2019-09-26 20:22:31 +0000

    test files and refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,9 @@
 const request = require('request');
 //const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
-const breed = process.argv[2];
 
-const fetchBreedDesc = function(breed) {
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => {
     const data = JSON.parse(body);
     let breedInfo = data[0];
@@ -11,20 +11,25 @@ const fetchBreedDesc = function(breed) {
       console.log('error');
     }
     if (error) {
-      console.log('error');
+      callback('error', null);
     }
           
     if (data) {
-      console.log(breedInfo.description);
+      callback(null, breedInfo.description);
     } else {
-      console.log("error: breed not found");
+      callback("error: breed not found", null);
     }
   });
 };
 
 
-fetchBreedDesc(breed);
 //const data = JSON.parse(body);
 //console.log(data);
 //console.log(typeof data);
 
+
+//fetchBreedDescription('Siberian', (error, description) {
+
+//});
+
+module.exports = { fetchBreedDescription };
